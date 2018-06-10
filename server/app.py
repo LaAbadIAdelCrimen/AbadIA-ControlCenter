48fc3b21169c8dfd8d495aa140fccd0fd43adde1
bazarum <juantomas.garcia@gmail.com>
2018-06-10 21:22:05 +0200

    first version of the game actions loader

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-
+import os
+import json
 
 # configuration
 DEBUG = True
@@ -17,6 +18,19 @@ CORS(app)
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+@app.route('/game', methods=['GET'])
+def game():
+    # filename = os.path.join("/tmp", "game.json"),
+    with open("/tmp/game.json") as gamefile:
+        data = json.load(gamefile)
+    # abadia_actions_180608_235540_290496.json
+    return jsonify({
+        'status': 'success',
+        'game': data
+    })
+
+
 
 
 if __name__ == '__main__':
