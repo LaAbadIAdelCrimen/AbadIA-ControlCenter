8d9cb9ded6c51f98022265ed1614cea79dd02af2
bazarum <juantomas.garcia@gmail.com>
2018-06-10 17:19:57 +0200

    Included axios en dump component

--- client/src/components/dump.vue
+++ client/src/components/dump.vue
@@ -1,16 +1,28 @@
-<template>
-  <div>
-    <p>{{ msg }}</p>
-  </div>
-</template>
-
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Dump',
-  data () {
+  data() {
     return {
-      msg: 'Dump Info from Game Engine'
+      msg: ''
     }
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/ping'
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error)
+        });
+    },
+  },
+  created() {
+    this.getMessage();
   }
-}
+};
 </script>
